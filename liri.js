c4a2bf777915e01722c04db69d4595a271c93955
Eric Steinberg <eric@MASTER.MSHOME>
2016-10-01 18:20:45 -0500

    Added Twitter, OBDB, and Spotify capability

--- liri.js
+++ liri.js
@@ -26,6 +26,68 @@ function run(){
 		// If we log that user as a JSON, we can see how it looks.
 		//console.log(JSON.stringify(user, null, 2));
 		if(menu.menu === "Twitter"){
+			inquirer.prompt([
+				// Here we create a basic text prompt.
+				{
+					type: "list",
+					message: "What tweets would you like to see?",
+					name: "person",
+					choices: ["Eric", "Hillary Clinton", "Donald Trump", "Gary Johnson", "Jill Stein"]
+				}
+			]).then(function (person) {
+				//realDonaldTrump
+				//HillaryClinton
+				//GovGaryJohnson
+				//DrJillStein
+				var handle;
+				switch(person.person){
+					case "Eric":
+						handle="CPUHobbes";
+						break;
+					case "Hillary Clinton":
+						handle="HillaryClinton";
+						break;
+					case "Donald Trump":
+						handle="realDonaldTrump";
+						break;
+					case "Gary Johnson":
+						handle="GovGaryJohnson";
+						break;
+					case "Jill Stein":
+						handle="DrJillStein";
+						break;
+				}
+				var client = new twitter(twitter_keys.twitterKeys);
+				var params = {screen_name: handle, count:'20'};
+				client.get('statuses/user_timeline', params, function(err, tweets, response) {
+			  		if (!err) {
+			  			outputStr="";
+			    		tweets.forEach(function(value, index){
+			    			outputStr+=("Tweet #"+(index+1)+"\nCreated: "+value.created_at+"\nMessage: ");
+			    			var messageArr = value.text.split(" ");
+			    			var lengthCount =0;
+			    			messageArr.forEach(function(value2, index2){
+			    				lengthCount+=value2.length+1; //length of word plus a space
+			    				if(lengthCount>60){
+			    					outputStr+="\n\t ";
+			    					lengthCount=0;
+			    				}
+			    				if(value2!="\n"){ //Catch new lines
+			    					outputStr+=value2+" ";
+			    				}
+			    			});
+			    			outputStr+="\n\n";
+			    		});
+			    		console.log(outputStr);
+			  		}
+			  		else{
+			  			console.log("There was a problem:\n"+err);
+
+			  		}
+			  		run();
+				});
+
+			});//console.log(JSON.stringify(client, null, 2))	
 
 		}
 
@@ -43,7 +105,7 @@ function run(){
 				spotify.search({ type: 'track', query: song.song }, function(err, info) {
 			    
 				    if ( err ) {
-				        console.log("There was a problem:\n"+error);
+				        console.log("There was a problem:\n"+err);
 				    }
 				    else{
 			    		var outputStr = "Artist: "+ info.tracks.items[0].artists[0].name+"\n"+
@@ -71,8 +133,8 @@ function run(){
 				//ADD ROTTEN TOMATOES INFO HERE WHEN RECEIVED!!!!!!!!!
 
 
-				request('http://www.omdbapi.com/?t='+movie.movie+"", function (error, response, info) {
-		  			if (!error && response.statusCode == 200) {
+				request('http://www.omdbapi.com/?t='+movie.movie+"", function (err, response, info) {
+		  			if (!err && response.statusCode == 200) {
 		  				var movieObj = JSON.parse(info);
 		  				if (movieObj.Response != "False"){
 			  				var outputStr = "Title: "+ movieObj.Title+"\n"+
@@ -92,7 +154,7 @@ function run(){
 	 				}
 	 				
 	 				else{
-	 					console.log("There was a problem:\n"+error);
+	 					console.log("There was a problem:\n"+err);
 
 	 				}
 	 			});
